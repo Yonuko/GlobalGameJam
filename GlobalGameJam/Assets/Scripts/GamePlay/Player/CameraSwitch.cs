10632d3273794d9b46ee23059a912bef795a80b8
Sacha <sacha.eghiazarian@ynov.com>
2020-02-01 21:51:49 +0100

    NTM
    COnnard

--- GlobalGameJam/Assets/Scripts/GamePlay/Player/CameraSwitch.cs
+++ GlobalGameJam/Assets/Scripts/GamePlay/Player/CameraSwitch.cs
@@ -8,11 +8,14 @@ public class CameraSwitch : MonoBehaviour
     public GameObject cameraTwo;
     public GameObject switchTransitionUI;
 
+    public GameObject playerOne;
+    public GameObject playerTwo;
+
     AudioListener audioOne;
     AudioListener audioTwo;
 
     private float startTime = 0f;
-    public float holdTime = 2.0f;       // Temps en seconde à tenir pour faire le switch de camera
+    public float holdTime = 1.0f;       // Temps en seconde à tenir pour faire le switch de camera
 
     private bool cameraSwitch = false;
     public bool isFishHome = false;     // Si true, le poisson est dans la main du perso et la camera ne peut pas switch
@@ -20,26 +23,27 @@ public class CameraSwitch : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        DontDestroyOnLoad(this);
+
         audioOne = cameraOne.GetComponent<AudioListener>();
         audioTwo = cameraTwo.GetComponent<AudioListener>();
 
         audioTwo.enabled = false;
         cameraTwo.GetComponent<Camera>().enabled = false;
+        playerTwo.GetComponent<PlayerMouvement>().stopMoving = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(1) && !isFishHome){
-            Debug.Log("Starting switch Timer");
+        if (Input.GetMouseButtonDown(0) && !isFishHome){
             startTime = Time.time;
         }
 
-        if (Input.GetMouseButton(1) && !isFishHome) // Changement de cam
+        if (Input.GetMouseButton(0) && !isFishHome) // Changement de cam
         {
             if (startTime + holdTime <= Time.time)
             {
-                Debug.Log("Finishing switch Timer");
 
                 switchTransitionUI.GetComponent<Animator>().Play("FadeIn", 0, 0);
                 StartCoroutine(delaySwitch());
@@ -48,7 +52,7 @@ public class CameraSwitch : MonoBehaviour
             }
         }
 
-        if( Input.GetMouseButtonUp(1) && !isFishHome)
+        if( Input.GetMouseButtonUp(0) && !isFishHome)
         {
             startTime = 0f;
         }
@@ -60,8 +64,15 @@ public class CameraSwitch : MonoBehaviour
         {
             audioTwo.enabled = false;
             cameraTwo.GetComponent<Camera>().enabled = false;
+            playerTwo.GetComponent<PlayerMouvement>().stopMoving = true;
+
             audioOne.enabled = true;
             cameraOne.GetComponent<Camera>().enabled = true;
+            playerOne.GetComponent<PlayerMouvement>().stopMoving = false;
+
+            //playerTwo.tag = "Untagged";
+            //playerOne.tag = "Player";
+            
 
             cameraSwitch = false;
         }
@@ -69,8 +80,14 @@ public class CameraSwitch : MonoBehaviour
         {
             audioTwo.enabled = true;
             cameraTwo.GetComponent<Camera>().enabled = true;
+            playerTwo.GetComponent<PlayerMouvement>().stopMoving = false;
+
             audioOne.enabled = false;
             cameraOne.GetComponent<Camera>().enabled = false;
+            playerOne.GetComponent<PlayerMouvement>().stopMoving = true;
+
+            //playerOne.tag = "Untagged";
+            //playerTwo.tag = "Player";
 
             cameraSwitch = true;
         }
---
+++ GlobalGameJam/Assets/Scripts/SceneManagement/MeIsDoor.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MeIsDoor : MonoBehaviour
+{
+    Animator anim;
+
+    
+
+    bool lastSeen = false; // false = left, true = right
+
+    public string nameSceneLeft;
+    public string nameSceneRight;
+
+    private bool outOfTrigger = true;
+    private bool animationEnded = false;
+
+    private void Start()
+    {
+        anim = transform.GetChild(0).GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if(outOfTrigger  && animationEnded)
+        {
+            animationEnded = false;
+            anim.Play("SlideDownDoor");
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (Input.GetKeyDown("e"))
+            {
+                LoadScene(nameSceneRight);
+                LoadScene(nameSceneLeft);
+                anim.Play("SlideUpDoor"); // && isHoldingFish
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            outOfTrigger = false;
+            LoadScene(nameSceneRight);
+            LoadScene(nameSceneLeft);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            outOfTrigger = true;
+        }
+    }
+
+    public void ExitDoor()
+    {
+        if (lastSeen)
+        {
+            UnloadScene(nameSceneLeft);
+        }
+        else
+        {
+            UnloadScene(nameSceneRight);
+        }
+    }
+
+    private void LoadScene(string sceneName)
+    {
+        if (!SceneManager.GetSceneByName(sceneName).isLoaded)
+            SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+    }
+
+    private void UnloadScene(string sceneName)
+    {
+        if (SceneManager.GetSceneByName(sceneName).isLoaded)
+            SceneManager.UnloadSceneAsync(sceneName);
+    }
+
+    public void lastSeenSetter(bool lastSeen)
+    {
+        this.lastSeen = lastSeen;
+    }
+    public void animSetter(bool laboule)
+    {
+        this.animationEnded = laboule;
+    }
+}
