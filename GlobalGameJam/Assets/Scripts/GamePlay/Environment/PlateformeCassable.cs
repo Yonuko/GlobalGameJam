7c762960db98f132ab92a983ce88cbd670bd4ba9
Thomas L <thomaslomb13@gmail.com>
2020-02-01 19:53:35 +0100

    Plateformes friables

---
+++ GlobalGameJam/Assets/Scripts/GamePlay/Environment/PlateformeCassable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlateformeCassable : MonoBehaviour
+{
+    Animator anim;
+
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if(hit.gameObject.tag == "PlateformeCassable")
+        {
+            hit.gameObject.tag = "Untagged";
+            anim = hit.gameObject.GetComponent<Animator>();
+            anim.Play("crumble");
+        }
+    }
+}
