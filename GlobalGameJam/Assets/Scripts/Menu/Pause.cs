5eeaef70719c1998fc3ebed3f1ea842da8bd540a
Thomas L <thomaslomb13@gmail.com>
2020-02-02 15:45:40 +0100

    Ajout Pause

---
+++ GlobalGameJam/Assets/Scripts/Menu/Pause.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    private bool pause = false;
+    void Update()
+    {
+        if (Input.GetKeyDown(Loader.get().datas.keys["Escape"]))
+        {
+            PauseManager();
+        }
+    }
+
+    public void PauseManager()
+    {
+        if (!pause)
+        {
+            pause = true;
+            Time.timeScale = 0f;
+            GameObject.Find("Pause").GetComponent<Canvas>().enabled = true;
+        }
+        else
+        {
+            pause = false;
+            Time.timeScale = 1f;
+            GameObject.Find("Pause").GetComponent<Canvas>().enabled = false;
+        }
+    }
+}
