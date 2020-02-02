2c6c9041b87d5b7ce29df1b84d9426fe26cb09d4
Thomas L <thomaslomb13@gmail.com>
2020-02-02 11:54:45 +0100

    Améliorations Menu

---
+++ GlobalGameJam/Assets/Scripts/Rendering/CameraRotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+    public float speed;
+
+    private void Update()
+    {
+        transform.Rotate(new Vector3(0, 0.3f, 0));
+    }
+}
